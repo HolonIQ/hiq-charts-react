7a6fef213f96c85a34ea47ecd29697dd61e77856
cliu032 <chauncely@gmail.com>
2021-03-19 20:06:07 +1100

    build from react-charts

---
+++ dist/components.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+export const HiqHeatmap = /*@__PURE__*/ createReactComponent('hiq-heatmap');
+export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
+//# sourceMappingURL=components.js.map
---
+++ src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'hiq-charts';
+
+
+
+export const HiqHeatmap = /*@__PURE__*/createReactComponent<JSX.HiqHeatmap, HTMLHiqHeatmapElement>('hiq-heatmap');
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
