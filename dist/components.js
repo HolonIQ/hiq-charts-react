1e58945dab7de8ac50703c7970a5b29ced1d9ecf
cliu032 <chauncely@gmail.com>
2021-03-19 20:57:59 +1100

    update gitignore

--- dist/components.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-/* tslint:disable */
-/* auto-generated react proxies */
-import { createReactComponent } from './react-component-lib';
-export const HiqHeatmap = /*@__PURE__*/ createReactComponent('hiq-heatmap');
-export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
-//# sourceMappingURL=components.js.map
