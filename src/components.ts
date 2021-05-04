76e75a05f2ffb77fb1581172ac68aeda987a683a
cliu032 <chauncely@gmail.com>
2021-05-04 23:13:19 +1000

    update build

--- src/components.ts
+++ src/components.ts
@@ -7,5 +7,7 @@ import type { JSX } from 'hiq-charts';
 
 
 
+export const HiqBeeswarm = /*@__PURE__*/createReactComponent<JSX.HiqBeeswarm, HTMLHiqBeeswarmElement>('hiq-beeswarm');
 export const HiqHeatmap = /*@__PURE__*/createReactComponent<JSX.HiqHeatmap, HTMLHiqHeatmapElement>('hiq-heatmap');
+export const HiqVerticalBarChart = /*@__PURE__*/createReactComponent<JSX.HiqVerticalBarChart, HTMLHiqVerticalBarChartElement>('hiq-vertical-bar-chart');
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
