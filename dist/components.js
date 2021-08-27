f7df060a175be564c7e451253327d498cc7f02f4
cliu032 <chauncely@gmail.com>
2021-08-27 19:11:15 +1000

    update build

---
+++ dist/components.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+import { defineCustomElements } from 'hiq-charts/loader';
+defineCustomElements();
+export const HiqBeeswarm = /*@__PURE__*/ createReactComponent('hiq-beeswarm');
+export const HiqHeatmap = /*@__PURE__*/ createReactComponent('hiq-heatmap');
+export const HiqTreemap = /*@__PURE__*/ createReactComponent('hiq-treemap');
+export const HiqVerticalBarChart = /*@__PURE__*/ createReactComponent('hiq-vertical-bar-chart');
+export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
+//# sourceMappingURL=components.js.map
