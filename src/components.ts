d48864d2b2fcb5f81248d1f481cd2f99610b69a7
cliu032 <chauncely@gmail.com>
2021-08-27 18:13:04 +1000

    add treemap

--- src/components.ts
+++ src/components.ts
@@ -1,13 +1,27 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from './react-component-lib';
+import { createReactComponent } from "./react-component-lib";
 
-import type { JSX } from 'hiq-charts';
+import type { JSX } from "hiq-charts";
 
-
-
-export const HiqBeeswarm = /*@__PURE__*/createReactComponent<JSX.HiqBeeswarm, HTMLHiqBeeswarmElement>('hiq-beeswarm');
-export const HiqHeatmap = /*@__PURE__*/createReactComponent<JSX.HiqHeatmap, HTMLHiqHeatmapElement>('hiq-heatmap');
-export const HiqVerticalBarChart = /*@__PURE__*/createReactComponent<JSX.HiqVerticalBarChart, HTMLHiqVerticalBarChartElement>('hiq-vertical-bar-chart');
-export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const HiqBeeswarm = /*@__PURE__*/ createReactComponent<
+  JSX.HiqBeeswarm,
+  HTMLHiqBeeswarmElement
+>("hiq-beeswarm");
+export const HiqHeatmap = /*@__PURE__*/ createReactComponent<
+  JSX.HiqHeatmap,
+  HTMLHiqHeatmapElement
+>("hiq-heatmap");
+export const HiqVerticalBarChart = /*@__PURE__*/ createReactComponent<
+  JSX.HiqVerticalBarChart,
+  HTMLHiqVerticalBarChartElement
+>("hiq-vertical-bar-chart");
+export const HiqTreeMap = /*@__PURE__*/ createReactComponent<
+  JSX.HiqTreemap,
+  HTMLHiqTreemapElement
+>("hiq-treemap");
+// export const MyComponent = /*@__PURE__*/ createReactComponent<
+//   JSX.MyComponent,
+//   HTMLMyComponentElement
+// >("my-component");
